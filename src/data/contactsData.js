d073d5a809230c603c347e0813139c07e3be4bee
Yashi Gupta <yashig52@gmail.com>
2024-01-14 00:04:48 +0530

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'yashig52@gmail.com',
-    phone: '+91 9696977133',
+    
     address: 'Kanpur, UttarPradesh',
 
     sheetAPI: 'https://sheetdb.io/api/v1/tt5b8x0x5sbmo'
